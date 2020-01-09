ebd7e9470e2202155723b662d76ba4039af2e3fb
prasta <prastamaha@gmail.com>
2020-01-09 15:30:46 +0800

    update

--- script.sh
+++ script.sh
@@ -1,40 +1,36 @@
 #!/bin/bash
 
-# open root
-echo 'change to root user'
-sudo su
-
 # update package
 echo 'update package'
-apt-get update
+sudo apt-get update
 
 # install openssh-server
 echo 'install openssh-server'
-apt-get install openssh-server
+sudo apt-get install openssh-server
 
 SSH_PORT=22
-read -p "whitch port will you use to ssh server? default[22] : " $SSH_PORT
+sudo read -p "whitch port will use for ssh server? default[22] : "$SSH_PORT
 
 if [ $SSH_PORT = 22 ]; then
     echo 'SSH PORT = '$SSH_PORT
-    echo  'Port '$SSH_PORT >> /etc/ssh/sshd_config
+    sudo echo  'Port '$SSH_PORT >> /etc/ssh/sshd_config
 else
     echo 'SSH PORT '$SSH_PORT
-    echo  'Port '$SSH_PORT >> /etc/ssh/sshd_config
+    sudo echo  'Port '$SSH_PORT >> /etc/ssh/sshd_config
 fi
 
 ROOT_LOGIN=n
-read -p "allow root login? yes [y], no [n] : " $ROOT_LOGIN
+sudo read -p "allow root login? yes [y], no [n] : "$ROOT_LOGIN
 
 if [ $ROOT_LOGIN = n ]; then
     echo 'disable root login'
-    echo 'PermitRootLogin no'
-elif [ $ROOT_LOGIN = y ]
+    sudo echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
+elif [ $ROOT_LOGIN = y ]; then
     echo 'enable root login'
-    echo 'PermitRootLogin yes'
+    sudo echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 else
     echo 'unknow char: disable root login'
-    echo 'PermitRootLogin no'
+    sudo echo 'PermitRootLogin no' >> /etc/ssh/sshd_config
 fi
 
 
